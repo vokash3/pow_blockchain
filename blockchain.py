c34f1659778f424f039a716cd81dd2b4ffdffb60
wain_m1 <vokash3@yandex.ru>
2022-09-25 12:19:39 +0300

    * Конкретизированы методы - Blockchain.new_block() - Blockchain.hash() * Добавлен блок генезиса в конструктор
    Реализовано ручное создание блоков

--- blockchain.py
+++ blockchain.py
@@ -1,18 +1,37 @@
+import json
+from datetime import datetime
+from hashlib import sha256
+
+
 class Blockchain(object):
 
     def __init__(self):
         self.chain = []  # начальнуя пустая цепочка списков (для хранения цепочки блоков)
         self.pending_transactions = []  # список задержанных транзакций
+        # Создает блок генезиса
+        print("Creating genesis block")
+        self.new_block()
 
-    def new_block(self):  # Генерирует новый блок и добавляет его в цепь
-        return None  # FIXME
+    def new_block(self, previous_hash=None):  # Генерирует новый блок и добавляет его в цепь
+        block: dict = {'index': len(self.chain), 'timestamp': datetime.utcnow().isoformat(),
+                       'transactions': self.pending_transactions, 'previous_hash': previous_hash, }
+        # Возвращает хэш этого нового блока и добавляет его в блок
+        block_hash = self.hash(block)
+        block["hash"] = block_hash
+        # Сбрасывает список незавершенных транзакций
+        self.pending_transactions = []
+        # Добавляет блок в цепочку
+        self.chain.append(block)
+        print(f"Created block {block['index']}")
+        return block
 
     @staticmethod
-    def hash(block):  # Hashes a Block pass
-        return None  # FIXME
+    def hash(block):  # Хэширование (sha256 дайджест) блока, который в json формате
+        block_string: bytes = json.dumps(block, sort_keys=True).encode()
+        return sha256(block_string).hexdigest()
 
-    def last_block(self):  # Получает последний блок в проходе цепочки
-        return None  # FIXME
+    def last_block(self):  # Возвращает последний блок в цепочке (если есть блоки)
+        return self.chain[-1] if self.chain else None
 
     def new_transaction(self, sender, recipient, amount):  # добавляет новую транзакцию в список задержанных транзакций
         self.pending_transactions.append({
